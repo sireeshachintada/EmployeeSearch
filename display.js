784b92b899661029822ec5500b8b6c55a27e727d
sireesha <sireesha.chintada@wipro.com>
2016-11-11 15:25:27 +0530

    clear rows

--- display.js
+++ display.js
@@ -1,27 +1,37 @@
+// Function which clears the previous results
+// Also to clear the table from the page, added a button to clear data which inturn calls DeleteRows()
+function DeleteRows() {
+  var table = document.getElementById("water");
+    var rowCount = table.rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+}
+
+// loads the data onto the table
 function loadData(){
             var data_file = "employee.json";
             var http_request = new XMLHttpRequest();
             try{
                // Opera 8.0+, Firefox, Chrome, Safari
                http_request = new XMLHttpRequest();
-            }catch (e){
+             }
+             catch (e){
                // Internet Explorer Browsers
                try{
                   http_request = new ActiveXObject("Msxml2.XMLHTTP");
-
-               }catch (e) {
-
+               }
+               catch (e){
                   try{
                      http_request = new ActiveXObject("Microsoft.XMLHTTP");
-                  }catch (e){
+                  }
+                  catch (e){
                      // Something went wrong
                      alert("Your browser broke!");
                      return false;
                   }
-
                }
             }
-
             http_request.onreadystatechange = function(){
                if (http_request.readyState == 4  ){
                   // Javascript function JSON.parse to parse JSON data
@@ -29,10 +39,10 @@ function loadData(){
 
                   // Name that is entered in the searchbox
                   var nameEntered = document.getElementById("name").value;
-
                   var table = document.getElementById("water");
                   var tbody = document.createElement("tbody");
                   var len = Object.keys(jsonObj).length; // Calculating length of Obj
+                  DeleteRows();
                   for(var i=0;i<len;i++){
                      var tr = document.createElement("tr");
                      var pattern = new RegExp(nameEntered, 'i');
@@ -55,10 +65,9 @@ function loadData(){
                    }
                }
             }
-
             http_request.open("GET", data_file, true);
             http_request.send();
          }
+
 // TODO
-// Clear the rows with removeChild
 // Print no recourds found
